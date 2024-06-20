8916897f4119f92d594b3e5c0d22d5873e74a945
Mike12041204 <104843870+Mike12041204@users.noreply.github.com>
2024-06-20 01:32:56 -0400

    Revert "Didn't work."
    This reverts commit e8662a8e9c96c5dd52fa6182cf341d1c87b57f6a.

--- src/common.cpp
+++ src/common.cpp
@@ -12,6 +12,39 @@ MPI_Request rq_send_msg[NUMBER_OF_PROCESSESS];          // array of handles for 
 MPI_Request rq_recv_msg[NUMBER_OF_PROCESSESS];
 bool global_free_list[NUMBER_OF_PROCESSESS];
 
+CPU_Graph::CPU_Graph(ifstream& graph_stream)
+{
+    graph_stream >> number_of_vertices;
+    graph_stream >> number_of_edges;
+    graph_stream >> number_of_lvl2adj;
+
+    onehop_neighbors = new int[number_of_edges];
+    onehop_offsets = new uint64_t[number_of_vertices + 1];
+    twohop_neighbors = new int[number_of_lvl2adj];
+    twohop_offsets = new uint64_t[number_of_vertices + 1];
+
+    for (int i = 0; i < number_of_edges; i++) {
+        graph_stream >> onehop_neighbors[i];
+    }
+    for (int i = 0; i < number_of_vertices + 1; i++) {
+        graph_stream >> onehop_offsets[i];
+    }
+    for (int i = 0; i < number_of_lvl2adj; i++) {
+        graph_stream >> twohop_neighbors[i];
+    }
+    for (int i = 0; i < number_of_vertices + 1; i++) {
+        graph_stream >> twohop_offsets[i];
+    }
+}
+
+CPU_Graph::~CPU_Graph() 
+{
+    delete onehop_neighbors;
+    delete onehop_offsets;
+    delete twohop_neighbors;
+    delete twohop_offsets;
+}
+
 DS_Sizes::DS_Sizes(const string& filename)
 {
     ifstream file(filename);
